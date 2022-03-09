0a4f71706b444e1221ad1772b31bdb5e63b2762e
Denver McCarthy <dmichaelspdx@gmail.com>
2022-03-09 15:03:37 -0800

    set up app.js with correct components and imports

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Footer from './components/Layout/Footer/Footer';
+import Header from './components/Layout/Header/Header';
+import Main from './components/View/Main/Main';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+
+      <Main />
+
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Presentational/Picker/Picker.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Picker() {
+  return <div>Picker</div>;
+}
